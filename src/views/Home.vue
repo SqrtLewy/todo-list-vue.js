b493820563e21aa044b174a4799dc69e181461bc
Kutasek111c <cramenpc@gmail.com>
2020-01-16 12:42:05 +0100

    add basic app form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,24 @@
 <template>
-<div class="home">
-<h1>Todo List</h1>
+<div class="container">
+<div class="app">
+<h2>Todo list</h2>
+<input v-model="Todo" placeholder="What do you have to do?" />
+<button>Add</button>
+<button>Clear list</button>
+<ul>
+<li v-for="todo in todos" v-bind:key="todo.id">{{ todo }}
+<label>
+<input type="checkbox" v-model="todo.complete">
+<span>&nbsp;</span>
+</label>
+<del v-if="todo.complete">{{ todo.text }}</del>
+<span v-else>{{ todo.text }}</span>
+</li>
+</ul>
+</div>
 </div>
 </template>
-
 <script>
-export default {
-  name: 'Home',
-};
 </script>
+<style lang="scss" scoped>
+</style>
