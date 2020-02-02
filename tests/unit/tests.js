5fb4ee51a11826c776068e19edf12bbb47064d62
Kutasek111c <cramenpc@gmail.com>
2020-02-02 23:48:26 +0100

    add example tests

--- tests/unit/tests.js
+++ tests/unit/tests.js
@@ -1,10 +1,15 @@
 import { mount } from '@vue/test-utils'
-import todoapp from '@/components/todoapp.vue';
+import todoapp from '@/components/todoapp.vue'
 
 describe('todoapp', () => {
   // Now mount the component and you have the wrapper
   const wrapper = mount(todoapp)
 
-  it('has a button', () => {
+it('site has a button', () => {
     expect(wrapper.contains('button')).toBe(true)
   })
+  
+it('site has a input', () => {
+    expect(wrapper.contains('input')).toBe(true)
+  })
+})
